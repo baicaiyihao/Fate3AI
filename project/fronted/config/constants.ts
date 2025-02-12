b1274319b7300318c4fdb12d72d65152dacd8721
baicaiyihao <stom698@163.com>
2025-02-12 14:24:52 +0800

    fix smart contract

--- project/fronted/config/constants.ts
+++ project/fronted/config/constants.ts
@@ -1,10 +1,10 @@
-export const TESTNET_FATE3AI_PACKAGE_ID = "0xe80c090d265c16c43cf37aa69544167c09bb028a078a9e9f11d9f008e0ee1ba5";
-export const TESTNET_AppTokenCap = "0x4c3163bc8a100fd31160ca0f1dad9cec0a5ca53c51907e3ca952fc8b040b26d3";
-export const TESTNET_Suipool = "0x27b130bc9d70a57d013d3a24f23d4bf4b86aaa5d9294fa8bdd8c2a18e8cbce21";
-export const TESTNET_PriceRecord = "0x1bf5df00fa8d5dbaece44e29f28f8933978138d743144113e437c097c65281cf";
-export const TESTNET_TokenRecord = "0x824e0a7466975aa8fba61f8a57020c34a20d3e757812c0a9e8630458b096292d";
-export const TESTNET_TokenPolicy = "0xc59e1ccfcd9520848cc1ec7652ffb13f8990e9daef0dc866317216de769e470f";
-export const TESTNET_RaffleInfo = "0x05529f9a6070768da33a3acf8975bfc8f9018c7bc3877b2216cec2df481dbede";
+export const TESTNET_FATE3AI_PACKAGE_ID = "0xb80aaf01f4dd8e785e7cd88a7f5b5bac2d5da07eba5eecf54a9c6e349eaa3e3b";
+export const TESTNET_AppTokenCap = "0x0f67c76b02fa5899e7cf16bf687b0e2589c4267e72dc91ef184fc9f732980d54";
+export const TESTNET_Suipool = "0x12863c00ab1976ee44a7e33bc9b9735e57bfaee755736ea55b5c8dc246f6ee90";
+export const TESTNET_PriceRecord = "0xaf641bbb4151aca8eb9046f77a42da69a1f41ab819dfb040534555d9b17d07fb";
+export const TESTNET_TokenRecord = "0x9d2a7e21e9378446e9f19f1488403944346ccfcf8d32d7d0b5173d85904541be";
+export const TESTNET_TokenPolicy = "0x1e4ac8492b9ae04482931b092a4a2d5c5acfc8f1bbc9960b4daf85fa7972f35c";
+export const TESTNET_RaffleInfo = "0xebd3b61a5dbb9e04d40291e706e07c35726edde8d73f903a442cabb8d0c509b6";
 export const TESTNET_TIME = "0x6";
 
 
